646e6d10efcaca7bb01e4b23655a1e399077525f
Adrian White <snarkyboojum@gmail.com>
2020-03-07 23:00:55 +1100

    Added notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+/*
+Summary of GCM -
+see https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf
+for more information.
+
+Authenticated encryption function
+
+Input data:
+ - plaintext, P
+ - additional authenticated data (AAD), A
+ - initialisation vector (IV), IV (basically a nonce)
+
+GCM protects the authenticity of both P, and A, and the confidentiality of P.
+
+len(P) <= 2.pow(39) - 256
+len(A) <= 2.pow(64) - 1
+1 <= len(IV) <= 2.pow(64) - 1
+
+GCM is defined on bit strings, where all strings are multiples of 8 (i.e. bytes)
+
+Output data:
+ - ciphertext, C (same bit length as P)
+ - authentication tag (tag), T (of length 128, 120, 112, 104, 96, or even 64, 32 bits)
+
+
+Authenticated decryption function
+
+Inputs:
+ - IV, A, C, T
+
+Outputs:
+ - P (plaintext corresponding to C)
+ - FAIL (if error)
+
+*/
+
 fn main() {
     println!(r#"Welcome to the GCM mode for AES (128)!"#);
 }
