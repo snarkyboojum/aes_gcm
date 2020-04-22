ff8fba229edcbce434e833d3a39ed8b39ead65fb
snarkyboojum <snarkyboojum@gmail.com>
2020-04-22 18:06:55 +1000

    Added further links and started the beginning of the algorithm

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This library provides a GCM mode for AES (currently AES-128).
 ## Resources
 
 - [Recommendation for Block Cipher Modes of Operation: Galois/Counter Mode (GCM) and GMAC](https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf)
+- [Cryptographic Algorithm Validation Program](https://csrc.nist.gov/Projects/cryptographic-algorithm-validation-program/CAVP-TESTING-BLOCK-CIPHER-MODES#GCMVS)
+    - [GCM Test Vectors](https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Algorithm-Validation-Program/documents/mac/gcmtestvectors.zip)
--- src/main.rs
+++ src/main.rs
@@ -38,36 +38,49 @@ Outputs:
 
 */
 
-enum Authenticity {
+pub enum Authenticity {
     Pass,
     Fail,
 }
 
 // multiplication operation on blocks, see p11 of Ref[1]
 // takes 128 bit blocks, builds the product and returns it (as a 128 bit block)
-fn mul_block(block1: &[u8], block2: &[u8], output: &mut [u8]) {}
+fn mul_block(x: &[u8], y: &[u8], output: &mut [u8]) {
+    assert_eq!(x.len(), 16);
+    assert_eq!(y.len(), 16);
+}
 
 // GHASH function, see p12 of Ref[1]
 // takes an integer m multiple of 128 bit strings, i.e. m x 128
 fn ghash(bit_string: &[u8], output: &mut [u8]) {}
 
 // we use the AES-128 bit cipher, see p13 of Ref[1]
-fn gctr(counter_block: &[u8], bit_string: &[u8], output: &mut [u8]) {}
+fn gctr(counter_block: &[u8], bit_string: &[u8], output: &mut [u8]) {
+    // TODO: parallelise as much as possible here - bit string into n * 128 bit blocks
+}
 
 // authenticated encryption algorithm, see p15 of Ref[1] - using AES-128
 // returns ciphertext and tag
-fn gcm_ae(
+pub fn gcm_ae(
     iv: &[u8],
     plaintext: &[u8],
     additional_data: &[u8],
     ciphertext: &mut [u8],
     tag: &mut [u8],
 ) {
+    // apply cipher to the "zero" block
+    let key_schedule = [0u32; 4 * (aes_crypt::Rounds::Ten as usize + 1)];
+    let hash_subkey = aes_crypt::cipher(&[0u8; 16], &key_schedule);
+
+    // generate the key from the IV
+    let counter_block = [0u8; 16];
+    let mut ciphertext = [0u8; 16];
+    gctr(&counter_block, plaintext, &mut ciphertext);
 }
 
 // authenticated decryption, see p16 of Ref[1] - using AES-128
 // returns plaintext and an Authenticity flag - Pass or Fail
-fn gcm_ad(
+pub fn gcm_ad(
     iv: &[u8],
     ciphertext: &[u8],
     additional_data: &[u8],
@@ -79,4 +92,31 @@ fn gcm_ad(
 
 fn main() {
     println!(r#"Welcome to the GCM mode for AES (128)!"#);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_mul_block() {}
+
+    #[test]
+    fn test_ghash() {}
+
+    #[test]
+    fn test_gctr() {}
+
+    #[test]
+    fn test_gcm_ae() {
+        /*
+        Key = c608316f809e3c54f3272a18256a5fec
+        IV = 38f4ec6b2c1c197bf6e0e994
+        CT = 659228b6282c2226c755136a9fc1bcacdc8cb640660cc784a841b5c385f34302a8bc5c0bd30b982d1b641bf642d958dddb3d46
+        AAD = d22804c6a53262ccd930946be718e465
+        Tag = ac9ed5212b5623d445d76a5f25e14e
+        PT = 2fc429740460dd0bea16bfe314d3258f6708b5ebb8ad2c4afd4d11fe99646227abe997f0688fc0e3f1c7c0462dc9254dbebfb0
+        */
+    }
+
+    #[test]
+    fn test_gcm_ad() {}
 }
