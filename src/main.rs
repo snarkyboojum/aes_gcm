0c5426454077846f2e09b44896987af1c9efc23a
snarkyboojum <snarkyboojum@gmail.com>
2020-04-26 14:45:02 +1000

    Playing with a byte-first approach

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,75 @@ pub enum Authenticity {
     Fail,
 }
 
+// this assume big endianess
+fn lsb(byte: u8) -> bool {
+    let lsb = byte & 1;
+    lsb == 1
+}
+
+fn msb(byte: u8) -> bool {
+    let msb = (byte >> 7) & 1;
+    msb == 1
+}
+
+fn get_bit(byte: u8, pos: usize) -> bool {
+    let bit = (byte >> pos) & 1;
+    bit == 1
+}
+
+fn print_bits(bytes: &[u8]) {
+    for b in bytes {
+        print!("{:b}", b);
+    }
+    print!("\n");
+}
+
+fn set_bit(byte: &mut u8, pos: usize, bit: bool) {}
+
 // multiplication operation on blocks, see p11 of Ref[1]
 // takes 128 bit blocks, builds the product and returns it (as a 128 bit block)
-fn mul_block(x: &[u8], y: &[u8], output: &mut [u8]) {
+//
+// TODO : currently implemented as an array of bytes, but should probably use
+// 127 bit usigned ints
+fn mul_block(x: &[u8; 16], y: &[u8; 16], output: &mut [u8; 16]) {
+    let mut z = [0u8; 16];
+    let mut v = *y;
+
+    let mut R = [0u8; 16];
+    R[1] = 0b11100001; // R is 11100001 || 0(120) as per spec[1]
+                       // print_bits(&R);
+
+    // looping 'bitwise', but over an array of bytes
+    for i in 0..128 {
+        let byte_offset = i / 8;
+        let bit_offset = i % 8;
+
+        let x_bit = get_bit(x[byte_offset], bit_offset);
+        let z_bit = get_bit(z[byte_offset], bit_offset);
+        let v_bit = get_bit(v[byte_offset], bit_offset);
+
+        if x_bit {
+            if bit_offset == 7 {
+                set_bit(&mut z[byte_offset + 1], 0, z_bit);
+            } else {
+                set_bit(&mut z[byte_offset], bit_offset + 1, z_bit);
+            }
+        } else {
+            // TODO: is this right? does xor'ing bool work?
+            if bit_offset == 7 {
+                set_bit(&mut z[byte_offset + 1], 0, z_bit ^ v_bit);
+            } else {
+                set_bit(&mut z[byte_offset], bit_offset + 1, z_bit ^ v_bit);
+            }
+        }
+
+        if !lsb(v[i]) {
+            v[i + 1] = v[i] >> 1;
+        }
+        if lsb(v[i]) {
+            // v[i + 1] = (v[i] >> 1) ^ R;
+        }
+    }
     assert_eq!(x.len(), 16);
     assert_eq!(y.len(), 16);
 }
@@ -96,6 +162,34 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn test_msb() {
+        let mut byte = 0b10011001;
+        assert_eq!(msb(byte), true);
+
+        byte = 0b01011001;
+        assert_eq!(msb(byte), false);
+    }
+
+    #[test]
+    fn test_lsb() {
+        let mut byte = 0b10011001;
+        assert_eq!(lsb(byte), true);
+
+        byte = 0b00011000;
+        assert_eq!(lsb(byte), false);
+    }
+
+    #[test]
+    fn test_get_bit() {
+        let byte = 0b10011010;
+        assert_eq!(get_bit(byte, 3), true);
+        assert_eq!(get_bit(byte, 7), true);
+        assert_eq!(get_bit(byte, 1), true);
+        assert_eq!(get_bit(byte, 0), false);
+    }
     #[test]
     fn test_mul_block() {}
 
